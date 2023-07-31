5956f5c0f316b0a0a93f0a6d1a76d6ace592118a
redtnx <red.tnx@gmail.com>
2023-07-31 15:53:20 +0700

    update

---
+++ src/Components/HomeBanner/HomeBanner.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { Carousel } from "antd";
+import axios from "axios";
+import { https } from "../../services/config";
+import { movieServ } from "../../services/movieServices";
+import { useState } from "react";
+const contentStyle = {
+  margin: 0,
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
+
+const HomeBanner = () => {
+  const [banner, setBanner] = useState([]);
+  const getAllBanner = async () => {
+    const res = await movieServ.getAllBanner();
+    console.log(res);
+    setBanner(res.data.content);
+  };
+
+  useEffect(() => {
+    getAllBanner();
+  }, []);
+
+  return (
+    <div>
+      <Carousel>
+        {banner.map((banner, index) => {
+          return (
+            <div key={index} className="h-70vh">
+              <img
+                className="w-full h-full object-cover"
+                src={banner.hinhAnh}
+              />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
+
+export default HomeBanner;
---
+++ src/services/movieServices.jsx
@@ -0,0 +1,6 @@
+import { https } from "./config";
+export const movieServ = {
+  getAllBanner: () => {
+    return https.get("/api/QuanLyPhim/LayDanhSachBanner");
+  },
+};
